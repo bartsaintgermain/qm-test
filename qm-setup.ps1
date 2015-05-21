2aeaddfe88f31e0d5cbaf4290ae72015468ae10d
Bart Saint Germain <bart.saintgermain@quamotion.mobi>
2015-05-21 15:53:08 +0200

    uncommenting download statements

--- qm-setup.ps1
+++ qm-setup.ps1
@@ -5,7 +5,7 @@ $rtkMSIFileName = "remotetestkit-en"
 
 $Source = "https://appkitbox.com/en/wp-content/download/remotetestkit-en.exe"
 $rtkMSIFile = "$PSScriptRoot\$rtkMSIFileName.exe"
-# Invoke-WebRequest $source -OutFile $rtkMSIFile
+Invoke-WebRequest $source -OutFile $rtkMSIFile
 
 $process = Start-Process "$rtkMSIFile" -ArgumentList "/SP /verysilent /suppressmsgboxes" -Wait -Passthru
 echo "Installer returned $($process.ExitCode)"
@@ -17,7 +17,7 @@ $qmMSIFileName = "Quamotion.Setup"
 
 $Source = "http://qmcdn.blob.core.windows.net/release-unstable/en-us/Quamotion.Setup.msi"
 $qmMSIFile = "$PSScriptRoot\$qmMSIFileName.msi"
-# Invoke-WebRequest $source -OutFile $qmMSIFile
+Invoke-WebRequest $source -OutFile $qmMSIFile
 
 # Initialize file names
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
